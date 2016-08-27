bf699eb2e55ecfaeaebda87b10a84cdd9dace62d
Alexey Lunacharsky <alun@katlex.com>
2016-08-27 12:39:13 +0100

    Switched to promise-fs

--- generate.js
+++ generate.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const co = require('co')
-const fs = require('fs')
+const fs = require('promise-fs')
 const child_process = require('child_process')
 
 'use strict';
@@ -11,27 +11,6 @@ function exec(cmd) {
     child_process.exec(cmd, (err, stdout, stderr) =>
       err && reject(err) || resolve([stdout, stderr])
     )
-  )
-}
-
-function writeFile(name, contents) {
-  return new Promise((resolve, reject) =>
-    fs.writeFile(name, contents, (err, result) =>
-    err && reject(err) || resolve(result))
-  )
-}
-
-function mkdir(name) {
-  return new Promise((resolve, reject) =>
-    fs.mkdir(name, (err, result) =>
-    err && reject(err) || resolve(result))
-  )
-}
-
-function readFile(name) {
-  return new Promise((resolve, reject) =>
-    fs.readFile(name, (err, data) =>
-    err && reject(err) || resolve(data.toString()))
   )
 }
 
@@ -51,7 +30,7 @@ if (cmd == 'config') {
 
   co(function * () {
     try {
-      yield writeFile('users.json', builtConfig)
+      yield fs.writeFile('users.json', builtConfig)
     }
     catch (e) {
       console.log(e)
@@ -64,21 +43,21 @@ else {
   const config = require('./' + confFile)
 
   function * prepareBitcoinConfig() {
-    yield mkdir('bitcoin').catch(() => {
+    yield fs.mkdir('bitcoin').catch(() => {
     })
     return yield config.map(user => co(function * () {
       const config = yield buildBitcoinConfig(user)
-      return yield writeFile('bitcoin/' + user.name, config)
+      return yield fs.writeFile('bitcoin/' + user.name, config)
     })).concat(co(buildMainConf))
   }
 
   function * buildMainConf() {
-    const mainConf = yield readFile('templates/server.conf')
-    return yield writeFile('bitcoin/bitcoin.conf', mainConf)
+    const mainConf = yield fs.readFile('templates/server.conf')
+    return yield fs.writeFile('bitcoin/bitcoin.conf', mainConf)
   }
 
   function * buildBitcoinConfig(user) {
-    const template = yield readFile('templates/client.conf')
+    const template = yield fs.readFile('templates/client.conf')
     return template
       .replace(/\${name}/g, user.name)
       .replace(/\${port}/g, user.port)
@@ -86,24 +65,24 @@ else {
 
   function * prepareStartFile() {
     const userServers = config.map(user => `bitcoind -conf=${user.name}`).join('\n')
-    const template = yield readFile('templates/run_servers')
-    yield writeFile('run_servers', template.replace(/\${userServers}/g, userServers))
+    const template = yield fs.readFile('templates/run_servers')
+    yield fs.writeFile('run_servers', template.replace(/\${userServers}/g, userServers))
     return yield exec('chmod +x run_servers')
   }
 
   function * prepareRunFile() {
-    const template = yield readFile('templates/run')
+    const template = yield fs.readFile('templates/run')
     const ports = config.map(user => `-p ${user.port}:${user.port}`).join(' ')
-    yield writeFile('s/run', template
+    yield fs.writeFile('s/run', template
       .replace(/\${ports}/, ports)
     )
   }
 
   function * prepareDockerFile() {
-    const template = yield readFile('templates/docker-file')
+    const template = yield fs.readFile('templates/docker-file')
     const names = config.map(user => user.name).join(' ')
     const ports = config.map(user => user.port).join(' ')
-    return yield writeFile('Dockerfile', template
+    return yield fs.writeFile('Dockerfile', template
       .replace(/\${names}/g, names)
       .replace(/\${ports}/g, ports)
       .replace(/\${confFile}/g, confFile)
@@ -111,11 +90,11 @@ else {
   }
 
   function * prepareCliScripts() {
-    const template = yield readFile('templates/cli-script')
+    const template = yield fs.readFile('templates/cli-script')
     const names = config.map(user => user.name)
-    yield mkdir('cli').catch(() => undefined)
+    yield fs.mkdir('cli').catch(() => undefined)
     return yield names.map(name => co(function * () {
-      yield writeFile(
+      yield fs.writeFile(
         `cli/${name}`,
         template.replace(/\${name}/g, name)
       )
